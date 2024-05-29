9b28d9d6e738364c1425c842b659bc5ddf4e7d75
Sheraz Akram <sherazakram.pro@gmail.com>
2024-05-29 21:07:59 +0500

    multer storage setup

--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,22 @@
 const express = require("express");
-const router = express.Router();
+const multer = require("multer");
+const path = require("path");
 const Blog = require("../models/blog");
+
+const router = express.Router();
+
+// multer storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(`./public/uploads/`));
+  },
+  filename: function (req, file, cb) {
+    const fileName = `${Date.now()}-${file.originalname}`;
+    cb(null, fileName);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router.get("/add-new", (req, res) => {
   res.render("addBlog", {
@@ -8,9 +24,15 @@ router.get("/add-new", (req, res) => {
   });
 });
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.redirect("/");
+router.post("/", upload.single("coverImage"), async (req, res) => {
+  const { title, body } = req.body;
+  const blog = await Blog.create({
+    title,
+    body,
+    coverImageURL: `/uploads/${req.file.filename}`,
+    createdBy: req.user._id,
+  });
+  res.redirect(`/blog/${blog._id}`);
 });
 
 module.exports = router;
