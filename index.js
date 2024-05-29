5cdb14b8296ca0509e2ac60c213110632e4bec32
Sheraz Akram <sherazakram.pro@gmail.com>
2024-05-29 15:00:50 +0500

    mongodb connection

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const userRoute = require("./routes/user");
+const mongoose = require("mongoose");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+const MongoURL = process.env.MONGODB_CONNECTION_STRING;
+
+mongoose
+  .connect(MongoURL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("Error", err));
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -14,7 +24,6 @@ app.get("/", (req, res) => {
 
 app.use("/user", userRoute);
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- models/user.js
+++ models/user.js
@@ -14,7 +14,6 @@ const userSchema = new mongoose.Schema(
     },
     salt: {
       type: String,
-      required: true,
     },
     password: {
       type: String,
